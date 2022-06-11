bdfa6b4a5a4760df85f145c8477f6fa310e85f5f
Luki Nowosad <lukinowoasd@gmial.com>
2022-06-11 14:21:24 +0200

    Ostateczna wersja przycisku

--- window.py
+++ window.py
@@ -1,92 +1,61 @@
-# klasa obsługująca przzycisk
-
+import copy
 import pygame
-vec =pygame.math.Vector2
+from cell import *
+vec = pygame.math.Vector2
+import copy
 
-class Button:
-    # inicjalizacja zmiennych
-    def __init__(self, surface, x, y, width, height, state = '', function = 0, color = (255, 255, 255), hover_color = (255, 255, 255),
-                 border = True, border_width = 2, border_color = (0, 0, 0), text = '', font_name = 'arial', text_size = 20,
-                 text_color = (0, 0, 0), bold_text = False):
-        self.x = x
-        self.y = y
-        self.pos = vec(x, y)
-        self.width = width
-        self.height = height
-        self.surface = surface
-        self.image = pygame.Surface((width, height))
+class Game_window:
+    def __init__(self, screen, x, y):
+        self.pos = vec(x,y)
+        self.screen = screen
+        self.width, self.height = 1200, 1000
+        self.image = pygame.Surface((self.width, self.height))
         self.rect = self.image.get_rect()
+        self.rows = 60
+        self.cols = 60
+        self.grafika = pygame.transform.scale(pygame.image.load(os.path.join("Grafika", "cell_alive.png")),(19,19))
+        self.grid = [[Cell(self.image, x, y, self.grafika) for x in range(self.cols)] for y in range(self.rows)]
+        for row in self.grid:
+            for cell in row:
+                cell.get_neigbours(self.grid)
+
+    def update(self):
         self.rect.topleft = self.pos
-        self.state = state
-        self.function = function
-        self.color = color
-        self.hover_color = hover_color
-        self.border = border
-        self.border_width = border_width
-        self.border_color = border_color
-        self.text = text
-        self.font_name = font_name
-        self.text_size = text_size
-        self.text_color = text_color
-        self.bold_text = bold_text
-        self.hovered = False
-        self.showing = True
-# ---------------------------------------------------------------------------------------------------------------------
-    #funckja wywołująca teskt na przyciku
-    def show_text(self):
-        font = pygame.font.SysFont(self.font_name, self.text_size, bold = self.bold_text)
-        text = font.render(self.text, False, self.text_color)
-        size = text.get_size()
-        x, y = self.width//2-(size[0]//2), self.height//2-(size[1]//2)
-        pos = vec(x,y)
-        self.image.blit(text, pos)
-# ---------------------------------------------------------------------------------------------------------------------
-    #funckja odpowiadająca za zmiany wyglądu przycisku
-    def update(self, pos, game_state = ''):
-        if self.mouse_hovering(pos):
-            self.hovered = True
-        else:
-            self.hovered = False
-        if self.state == '' or game_state == '':
-            self.showing = True
-        else:
-            if self.state == game_state:
-                self.showing = True
-            else:
-                self.showing = False
-# ---------------------------------------------------------------------------------------------------------------------
-    # funkcja zajmująca się przyciskaniem przycisku
+        for row in  self.grid:
+            for cell in row:
+                cell.update()
+
     def draw(self):
-        if self.showing:
-            if self.border:
-                self.image.fill(self.border_color)
-                if self.hovered:
-                    pygame.draw.rect(self.image, self.hover_color, (self.border_width, self.border_width,
-                                                                    self.width - (self.border_width * 2),
-                                                                    self.height - (self.border_width * 2)))
+        self.image.fill((102, 102, 102))
+        for row in  self.grid:
+            for cell in row:
+                cell.draw()
+        self.screen.blit(self.image, (self.pos.x, self.pos.y))
+
+    def reset_grid(self):
+        self.grid = [[Cell(self.image, x, y,self.grafika) for x in range(self.cols)] for y in range(self.rows)]
+        for row in self.grid:
+            for cell in row:
+                cell.get_neigbours(self.grid)
+
+    def evaluate(self):
+        new_grid = copy.copy(self.grid)
+
+        for row in self.grid:
+            for cell in row:
+                cell.live_neighbours()
+
+        for yidx, row in enumerate(self.grid):
+            for xidx, cell in enumerate(row):
+                if cell.alive:
+                    if cell.alive_neighbours == 2:
+                        new_grid[yidx][xidx].alive = True
+                    if cell.alive_neighbours < 2:
+                        new_grid[yidx][xidx].alive = False
+                    if cell.alive_neighbours > 3:
+                        new_grid[yidx][xidx].alive = False
                 else:
-                    pygame.draw.rect(self.image, self.color, (self.border_width, self.border_width,
-                                                                    self.width - (self.border_width * 2),
-                                                                    self.height - (self.border_width * 2)))
+                    if cell.alive_neighbours == 3:
+                        new_grid[yidx][xidx].alive = True
 
-            else:
-                self.image.fill(self.color)
-            if len(self.text) > 0:
-                self.show_text()
-            self.surface.blit(self.image, self.pos)
-
-# ---------------------------------------------------------------------------------------------------------------------
-    def mouse_hovering(self, pos):
-        if self.showing:
-            if pos[0] > self.pos[0] and pos[0] < self.pos[0] + self.width:
-                if pos[1] > self.pos[1] and pos[1] < self.pos[1] + self.height:
-                    return True
-            else:
-                return False
-        else:
-            return False
-
-# ---------------------------------------------------------------------------------------------------------------------
-    def click(self):
-        if self.function != 0 and self.hovered :
-            self.function()
+        self.grid = new_grid
